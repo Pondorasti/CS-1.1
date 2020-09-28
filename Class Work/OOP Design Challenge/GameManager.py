d924091e0cb89bd02115d10559fca6ecb6c1b520
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-09-27 17:47:14 -0700

    GameManager - Added Introduction Dialog

--- Class Work/OOP Design Challenge/GameManager.py
+++ Class Work/OOP Design Challenge/GameManager.py
@@ -1,3 +1,5 @@
+from os import system
+
 class GameManager:
     def __init__(self):
         map = []
@@ -11,8 +13,27 @@ class GameManager:
         self.map = map
         
     def start(self):
-        print(self.map)
-    
+        self.play_introduction_dialog()
+
+    def play_introduction_dialog(self):
+        system('clear')
+        print("Welcome to the dungeon!")
+        print("You are tasked with finding the exit and getting out alive.")
+        print()
+        print("Type 'start' to begin your adventure")
+        print("Type 'tutorial' to get a brief introduction")
+
+        
+        while(True):
+            command = input().lower()
+            
+            if command == 'tutorial':
+                print(command)
+            elif command == 'start':
+                print(command)
+            else:
+                print("That is not a valid command.")
+        
 
     def print_map(self):
         def print_pipes():
@@ -31,5 +52,34 @@ class GameManager:
             print_row(row)
         print_pipes()
 
+# GameManager().start()
 GameManager().print_map()
 
+
+# -----------
+# |0|0|0|0|0|
+# -----------
+# |0|0|0|0|0|
+# -----------
+# |0|0|0|0|0|
+# -----------
+# |0|0|0|0|0|
+# -----------
+# |0|0|0|0|0|
+# -----------
+
+# D0000
+# 00000
+# 0M0M0
+# 00000
+# 00H00
+# C0000
+
+
+# -------------
+# | 0 0 0 0 0 |
+# |------ 0 0 |
+# | 0 M | M 0 |
+# | 0 0 | 0 0 |
+# | 0 0 H 0 0 |
+# -------------
