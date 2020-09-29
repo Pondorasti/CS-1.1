ae6c73850558d7235472f6f38d33affbeabf24b8
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-09-28 22:18:05 -0700

    Finished CommandLine Library

--- Class Work/OOP Design Challenge/CommandLine.py
+++ Class Work/OOP Design Challenge/CommandLine.py
@@ -6,35 +6,44 @@ class CommandLine(ABC):
 
     @classmethod
     def parse(cls, input):  
+        if input == "help":
+            print("\nHere's a list of possible commands:\n")
+            for key in cls.__commands:
+                print(key)
+            return 
+
         arguments = input.split(" ")
-        if arguments[0] in cls.__commands:
-            if len(arguments) < 2:
-                print("Show help")
+        if arguments[0] in cls.__commands: # Check if command exists
+            key = arguments[0]
+            if len(arguments) < 2: # Show help if there's no options
+                cls.show_help(key)
                 return 
             
-            key = arguments[0]
             option = arguments[1]
-
-            if option.startswith("--", 0, 2):
+            if option.startswith("--", 0, 2): # Check for 'option_name'
                 for command in cls.__commands[key]:
                     if command['option_name'] == option:
                         command['action']()
                         return 
-
-                print(f"Unknown argument for command '{key}'.")
-            elif option.startswith("-", 0, 1):
+                
+                if option == "--help":
+                    cls.show_help(key)
+                else:
+                    print(f"Unknown argument for command '{key}'.")
+            elif option.startswith("-", 0, 1): # Check for 'short_option_name'
                 for command in cls.__commands[key]:
-                    if command['option_name'] == option:
+                    if command['short_option_name'] == option:
                         command['action']()
                         return
                         
-                print(f"Unknown argument for command '{key}'.")
+                if option == "-h":
+                    cls.show_help(key)
+                else:
+                    print(f"Unknown argument for command '{key}'.")
             else:
                 print(f"Unknown argument for command '{key}'.")
         else:
             print("Command not found!")
-        
-        # pass
 
     @classmethod
     def add_argument(cls, command_name: str, short_option_name: str, option_name: str, help: str, action):
@@ -52,3 +61,39 @@ class CommandLine(ABC):
         
         cls.__commands[command_name] = []
         cls.__commands[command_name].append(new_command)
+
+    @classmethod
+    def show_short_help(cls, key):
+        print(f"\nusage: {key}", end = " ")
+        for command in cls.__commands[key]:
+            print(f"[{command['short_option_name']} | {command['option_name']}]", end = " ")
+        print()
+    
+    @classmethod 
+    def show_help(cls, key):
+        print("\nHere's a list of possible commands:\n")
+        for command in cls.__commands[key]:
+            print(f"[{command['short_option_name']} | {command['option_name']}]        {command['help']}")
+        print()
+
+
+
+# Janky Unit Testing
+
+# def hello():
+#     print("Hello dear friend")
+
+# def random_int():
+#     print(5)
+
+# def stop():
+#     print("I don't want this to end!")
+
+# CommandLine.add_argument("say", "-h", "--hello", "Say hello to your friend", hello)
+# CommandLine.add_argument("say", "-r", "--random", "Returns a random integer", random_int)
+# CommandLine.add_argument("kill", "-m", "--me", "Stops executions of the program", stop)
+
+
+# while(True):
+#     command = input()
+#     CommandLine.parse(command)
