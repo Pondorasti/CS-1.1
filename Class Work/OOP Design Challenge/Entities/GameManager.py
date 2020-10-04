1189b1a6587596fa9dae01a7ba7403de427cdc61
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-10-03 21:42:14 -0700

    Improved Access Control

--- Class Work/OOP Design Challenge/Entities/GameManager.py
+++ Class Work/OOP Design Challenge/Entities/GameManager.py
@@ -5,9 +5,9 @@ from Map import Map
 class GameManager:
     def __init__(self):        
         self.map = Map()
-        
+  
     def start(self):
-        self.play_introduction_dialog()
+        self._play_introduction_dialog()
         
         while(True):
             print("\nIt's your turn")
@@ -19,23 +19,27 @@ class GameManager:
             system('clear')
             CommandLine.parse(command)
 
-    def play_introduction_dialog(self):
+    def _play_introduction_dialog(self):
         system('clear')
         print("Welcome to the dungeon!")
         print("You are tasked with finding the exit and getting out alive.")
         print()
         print("Type 'start' to begin your adventure")
         print("Type 'tutorial' to get a brief introduction")
+        print()
+
+        command = input().lower()
+
+        if command == 'tutorial':
+            print("Jokes on you, there's no tutorial")
+            print("Game starting soon...")
+            print("...")
+            print("...")
+        elif command == 'start':
+            print("Fine I will start the game...")
+        else:
+            print("Not sure what you meant, but game is starting...")
         
-        # while(True):
-        #     command = input().lower()
-        #     CommandLine.parse(command)
-            
-        #     if command == 'tutorial':
-        #         print(command)
-        #     elif command == 'start':
-        #         print(command)
-        #     else:
-        #         print("That is not a valid command.")
+        system('clear')
         
 GameManager().start()
--- Class Work/OOP Design Challenge/Entities/Map.py
+++ Class Work/OOP Design Challenge/Entities/Map.py
@@ -9,7 +9,7 @@ class Map:
     def __init__(self):
         map_number = randint(1, 3)
         file = open(f"map{map_number}.txt", "r")
-        
+
         rows = 0
         map = []
         for line in file:
@@ -20,10 +20,10 @@ class Map:
                     map[rows].append(Tile(rows, columns, "0"))
                 elif cell == 'H':
                     self._hero = Hero(rows, columns)
-                    CommandLine.add_argument("move", "-n", "--north", "Moves your character forward", self.move_forward_hero)
-                    CommandLine.add_argument("move", "-s", "--south", "Moves your character backward", self.move_backward_hero)
-                    CommandLine.add_argument("move", "-w", "--west", "Moves your character left", self.move_left_hero)
-                    CommandLine.add_argument("move", "-e", "--est", "Moves your character right", self.move_right_hero)
+                    CommandLine.add_argument("move", "-n", "--north", "Moves your character forward", self._move_forward_hero)
+                    CommandLine.add_argument("move", "-s", "--south", "Moves your character backward", self._move_backward_hero)
+                    CommandLine.add_argument("move", "-w", "--west", "Moves your character left", self._move_left_hero)
+                    CommandLine.add_argument("move", "-e", "--est", "Moves your character right", self._move_right_hero)
                     map[rows].append(self._hero)
                 elif cell == 'M':
                     map[rows].append(Monster(rows, columns))
@@ -38,48 +38,48 @@ class Map:
             rows += 1
 
         file.close()
-        self.number_of_rows = rows
-        self.number_of_columns = len(map[0])
-        self.map = map
+        self._number_of_rows = rows
+        self._number_of_columns = len(map[0])
+        self._map = map
 
     def print_map(self):
-        for row in range(self.number_of_rows):
-            for column in range(self.number_of_columns):
-                print(f"{self.map[row][column].get_short_name()}", end="")
+        for row in range(self._number_of_rows):
+            for column in range(self._number_of_columns):
+                print(f"{self._map[row][column].get_short_name()}", end="")
             print()
 
-    def move_forward_hero(self):
+    def _move_forward_hero(self):
         nextX = self._hero._x - 1
         nextY = self._hero._y 
-        self.move_hero(nextX, nextY, self._hero._x, self._hero._y)
+        self._move_hero(nextX, nextY, self._hero._x, self._hero._y)
 
-    def move_backward_hero(self):
+    def _move_backward_hero(self):
         nextX = self._hero._x + 1
         nextY = self._hero._y 
-        self.move_hero(nextX, nextY, self._hero._x, self._hero._y)
+        self._move_hero(nextX, nextY, self._hero._x, self._hero._y)
 
-    def move_left_hero(self):
+    def _move_left_hero(self):
         nextX = self._hero._x 
         nextY = self._hero._y - 1
-        self.move_hero(nextX, nextY, self._hero._x, self._hero._y)
+        self._move_hero(nextX, nextY, self._hero._x, self._hero._y)
     
-    def move_right_hero(self):
+    def _move_right_hero(self):
         nextX = self._hero._x 
         nextY = self._hero._y + 1
-        self.move_hero(nextX, nextY, self._hero._x, self._hero._y)
+        self._move_hero(nextX, nextY, self._hero._x, self._hero._y)
         
-    def move_hero(self, nextX, nextY, x, y):
-        if self.is_tile_free(nextX, nextY):
+    def _move_hero(self, nextX, nextY, x, y):
+        if self._is_tile_free(nextX, nextY):
             self._hero.move_to(nextX, nextY)
-            self.map[nextX][nextY] = Hero(nextX, nextY)
-            self.map[x][y] = Tile(nextX, nextY, "0")
+            self._map[nextX][nextY] = Hero(nextX, nextY)
+            self._map[x][y] = Tile(nextX, nextY, "0")
             self.print_map()
         else:
             self.print_map()
             print("You can only move on empty tiles.")
     
-    def is_tile_free(self, x: int, y: int):
-        if self.map[x][y]._shorthand_name == '0':
+    def _is_tile_free(self, x: int, y: int):
+        if self._map[x][y]._shorthand_name == '0':
             return True
         return False
 
